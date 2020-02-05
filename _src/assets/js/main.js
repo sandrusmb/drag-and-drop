c9a5c548bf8a762f59461f05367dbf527212c907
sandrusmb <sandrusmb@gmail.com>
2020-02-05 15:49:50 +0100

    Add basic structure and design

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,23 @@
-'use strict';
+/* "use strict"; */
 
-console.log('>> Ready :)');
+const list_items = document.querySelectorAll(".list-items");
+const lists = document.querySelectorAll(".list");
+
+let draggedItem = null;
+
+for (let i = 0; i < list_items.length; i++) {
+  const item = list_items[i];
+
+  item.addEventListener("dragstart", function(e) {
+    console.log("dragstart");
+    draggedItem = this;
+  });
+
+  item.addEventListener("dragend", function() {
+    console.log("dragend");
+  });
+
+  for (let j = 0; j < lists.length; j++) {
+    const list = lists[j];
+  }
+}
