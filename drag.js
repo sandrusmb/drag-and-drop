7a646d92e55ad1fdbf50e6766897b8fb661b535a
sandrusmb <sandrusmb@gmail.com>
2020-02-05 16:51:28 +0100

    wip

---
+++ drag.js
@@ -0,0 +1,35 @@
+const list_item = document.querySelectorAll(".list-item");
+
+const lists = document.querySelectorAll(".list");
+
+let draggedItem = null;
+
+for (let i = 0; i < list_item.length; i++) {
+  const item = list_item[i];
+
+  item.addEventListener("dragstart", function(ev) {
+    draggedItem = item;
+    setTimeout(function() {
+      item.style.display = "none";
+    }, 0);
+  });
+
+  item.addEventListener("dragend", function(ev) {
+    setTimeout(function() {
+      item.style.display = "block";
+      draggedItem = null;
+    }, 0);
+  });
+
+  for (let j = 0; j < lists.length; j++) {
+    const list = lists[j];
+
+    list.addEventListener("dragover", function(ev) {
+      ev.preventDefault();
+    });
+
+    list.addEventListener("drop", function(ev) {
+      this.append(draggedItem);
+    });
+  }
+}
